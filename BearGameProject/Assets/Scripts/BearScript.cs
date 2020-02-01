5a65a0e75bfca203a2380c8867f6940e63f2b28a
Yvonne <veeyren@gmail.com>
2020-02-01 11:33:09 +0100

    Simple bear prefab and script
    Just created a basic bear setup and script

---
+++ BearGameProject/Assets/Scripts/BearScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearScript : MonoBehaviour
+{
+    public GameObject Head;
+    public GameObject LeftArm;
+    public GameObject RightArm;
+    public GameObject LeftLeg;
+    public GameObject RightLeg;
+
+    void SetupBearParameters(bool showHead, bool showLeftArm, bool showRightArm, bool showLeftLeg, bool showRightLeg)
+    {
+        if(Head != null)
+        {
+            Head.SetActive(showHead);
+        }
+
+        if (LeftArm != null)
+        {
+            LeftArm.SetActive(showLeftArm);
+        }
+
+        if (RightArm != null)
+        {
+            RightArm.SetActive(showRightArm);
+        }
+
+
+        if (LeftLeg != null)
+        {
+            LeftLeg.SetActive(showRightLeg);
+        }
+
+        if (RightLeg != null)
+        {
+            RightLeg.SetActive(RightLeg);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
