4d4f5c1b470dfc2097f76312ff1766525d99f82c
Liljan <marcus.t.lilja@gmail.com>
2020-02-01 10:31:43 +0100

    Pull request test

--- BearGameProject/Assets/Scripts/EventTriggerTest.cs
+++ BearGameProject/Assets/Scripts/EventTriggerTest.cs
@@ -6,23 +6,15 @@ public class EventTriggerTest : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown("q"))
-        {
             EventManager.TriggerEvent("test");
-        }
 
         if(Input.GetKeyDown("o"))
-        {
             EventManager.TriggerEvent("Spawn");
-        }
 
         if(Input.GetKeyDown("p"))
-        {
             EventManager.TriggerEvent("Destroy");
-        }
 
         if(Input.GetKeyDown("x"))
-        {
             EventManager.TriggerEvent("Junk");
-        }
     }
 }
